5cc4c843e0faa32027f28bfcdb872d4947fed1b4
mahima <mahiworknov29@gmail.com>
2024-08-17 12:40:40 +0530

    Backend Code

---
+++ 1-simple-server/Readme.md
@@ -0,0 +1,6 @@
+1. npm init -y ======for creating package.json //Installing Packages--
+2. npm i express dotenv nodemon ====for express, dotenv nodemon //Installing Packages--
+
+3. .env file created
+4. .gitignore file created
+5. index.js file created
---
+++ 1-simple-server/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+require("dotenv");
+
+const app = express();
+const users = {
+  id: 1,
+  name: "Leanne Graham",
+  username: "Bret",
+  email: "Sincere@april.biz",
+  address: {
+    street: "Kulas Light",
+    suite: "Apt. 556",
+    city: "Gwenborough",
+    zipcode: "92998-3874",
+    geo: {
+      lat: "-37.3159",
+      lng: "81.1496",
+    },
+  },
+  phone: "1-770-736-8031 x56442",
+  website: "hildegard.org",
+  company: {
+    name: "Romaguera-Crona",
+    catchPhrase: "Multi-layered client-server neural-net",
+    bs: "harness real-time e-markets",
+  },
+};
+app.get("/", (req, res) => {
+  res.send("Home Route");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Contact Route");
+});
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is listening on Port ${PORT}`);
+});
